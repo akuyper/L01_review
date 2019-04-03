2f760e89e5b066dd34e14f7161d3e214992c49d3
j3schaue <jake.schauer@gmail.com>
2019-04-03 15:31:41 -0500

    added starter code and exercise

--- starter_script.R
+++ starter_script.R
@@ -10,15 +10,31 @@ wildfire_dat <- read_csv("data/wildfires.csv") %>%
   mutate(winddir = factor(winddir, levels = c("N", "NE", "E", "SE", "S", "SW", "W", "NW")),
          traffic = factor(traffic, levels = c("lo", "med", "hi")))
 
-# Candidate models
+###----------------------------------###
+# Candidate models for Exercise 1
+###----------------------------------###
 
 # Model 1
-mod01_fn <- function(df){
+mod01_burned_fn <- function(df){
   lm(burned ~ . - wlf, data = df)
 }
 
 # Model 2
-mod02_fn <- function(df){
+mod02_burned_fn <- function(df){
   lm(burned ~ poly(windspd, 3) + poly(rain, 3) + poly(vulnerable, 3) + heli, data = df)
 }
 
+
+###----------------------------------###
+# Candidate models for Exercise 3
+###----------------------------------###
+
+# Model 1
+mod01_wlf_fn <- function(df){
+  glm(wlf ~ . - wlf, data = df, family = binomial)
+}
+
+# Model 2
+mod02_wlf_fn <- function(df){
+  glm(wlf ~ poly(windspd, 2) + winddir + poly(rain, 2) + poly(vulnerable, 3) + x*y, data = df)
+}
